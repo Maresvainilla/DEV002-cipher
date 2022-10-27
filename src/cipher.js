922e4f6db9a873a80ba14f11fce59d5b0ae5ed14
Maresvainilla <bris1208@hotmail.com>
2022-10-27 11:26:59 -0500

    funcion cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
-const cipher = {
+function cipher(){
+function inicio() {
+  document.getElementById("mensaje").addEventListener("keyup",function(){
+    this.value=this.value.toUppercase();
+  },true); 
+  document.getElementById("cifrar").addEventListener("click",function(){
+    let texto=document.getElementById("mensaje").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("mensaje2").value = cifrar(texto,desplazamiento);
+  },true);
+  document.getElementById("descifrar").addEventListener("click",function(){
+    let texto=document.getElementById("mensaje").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("mensaje2").value = descifrar(texto,desplazamiento);
+  },true);
+
+
   // ...
 };
+
+
+function cifrado(texto,desplazamiento){
+  let resultado ="";
+  let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  desplazamiento=(desplazamiento%26+26)%26;
+  if(texto){
+    for (let i=0; i<texto.length; i++){
+      if(letras.indexOf(texto[i])!=-1){
+        let posicion =((lestras.indexOf(texto[i])+desplazamiento)%26);
+        resultado +=letras[posicion];   
+      }
+    else
+        resultado += texto[i];
+   }    
+  }
+    return resultado;
+}
+    
+}
+
+
 
 export default cipher;
